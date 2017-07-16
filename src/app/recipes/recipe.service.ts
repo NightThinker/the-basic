6ed0803b7cce98a998c7b943d57c541739a14fc5
NightTinker <travel_special@hotmail.com>
2017-07-17 02:58:51 +0700

    10.Service - Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,27 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[]= [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg')
+    new Recipe(
+      'Testy Schnitzel',
+      'A super-testy Xhhnitzal - just awesome!',
+      'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg',
+      [
+        new Ingredient('Salmon', 1),
+        new  Ingredient('Lemon', 1)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://s-media-cache-ak0.pinimg.com/originals/da/55/e3/da55e32da25738ecde20582ec6416a2f.jpg',
+      [
+        new Ingredient('Meat', 2),
+        new Ingredient('Onion', 1),
+        new Ingredient('Bacon', 5)
+      ])
   ];
 
   getRecipes() {
