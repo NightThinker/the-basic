2488e3d02e31e83a4c9ec8c0e791d0e1225d671b
NightTinker <travel_special@hotmail.com>
2017-07-17 00:25:28 +0700

    9.service - Managing Recipes in a RecipeService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,11 @@
+import {Recipe} from './recipe.model';
 export class RecipeService {
+  private recipes: Recipe[]= [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg')
+  ];
 
+  getRecipes() {
+    return this.recipes.slice()
+  }
 }
